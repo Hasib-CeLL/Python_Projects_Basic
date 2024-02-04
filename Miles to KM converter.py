538a07e232db097994bac0ec13a25870caa008d6
Hasibur Rahman <134851315+Hasib-CeLL@users.noreply.github.com>
2024-02-04 22:25:55 +0600

    Add files via upload

---
+++ Miles to KM converter.py
@@ -0,0 +1,12 @@
+distance1_m = 20
+distance2_m = 30
+distance3_m = 40
+def miles_to_km(distance_m) :
+    distance_km = distance_m * 1.6
+    print("The distance is: ")
+    print(distance_km, "kilo meter")
+
+miles_to_km(distance1_m)
+miles_to_km(distance2_m)
+miles_to_km(distance3_m)
+
